9d58ba5ac57742dca52ab65be2b71c69738c8574
wabohorquezr <wabohorquezr@gmail.com>
2025-06-23 18:24:09 -0500

    laboratorio 3

--- Control/Tareas/Actividadess/trabajo.jl
+++ Control/Tareas/Actividadess/trabajo.jl
@@ -1,1 +1,11 @@
-:
+include("ControlUN.jl")
+using LinearAlgebra
+A = [-6 -3.5;
+      6  4]
+B=[0; 1]
+# Localización deseada
+polos=[-3, -1]
+# Calcula matriz de ganancia para localizar los polos
+G=place(A,B,polos)
+ALC = A - B*G
+eigen(ALC)
